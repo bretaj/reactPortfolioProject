519d7083cd214ecc6d4ce063e00490929e3d2b94
bret jensen <jensenbret13@gmail.com>
2025-02-23 11:02:01 -0800

    created components files

---
+++ reactPortfolioProject/src/components/footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
+        <ul className="social-links">
+          <li>
+            <a href="#" target="_blank" rel="">
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank" rel="">
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank" rel="">
+              Twitter
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
