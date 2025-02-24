d0cfc01ac279e94dcbe8ff93ab892354542502c8
bret jensen <jensenbret13@gmail.com>
2025-02-23 19:43:19 -0800

    .

--- reactPortfolioProject/src/components/footer.jsx
+++ reactPortfolioProject/src/components/footer.jsx
@@ -1,29 +1,8 @@
 
 function Footer () {
-  return (
-    <footer className="footer">
-      <div className="footer-content">
-        <p>&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
-        <ul className="social-links">
-          <li>
-            <a href="#" target="_blank" rel="">
-              GitHub
-            </a>
-          </li>
-          <li>
-            <a href="#" target="_blank" rel="">
-              LinkedIn
-            </a>
-          </li>
-          <li>
-            <a href="#" target="_blank" rel="">
-              Twitter
-            </a>
-          </li>
-        </ul>
-      </div>
-    </footer>
-  );
+  return {
+
+  };
 };
 
 export default Footer;
--- reactPortfolioProject/src/components/header.jsx
+++ reactPortfolioProject/src/components/header.jsx
@@ -1,10 +1,25 @@
 function Header () {
-    return {
-        
-    }
-}
-
-
-
+    return (
+        <header>
+            <h1>My Portfolio</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">About Me</Link>
+                    </li>
+                    <li>
+                        <Link to="/portfolio">Portfolio</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    <li>
+                        <Link to="/resume">Resume</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
 
 export default Header;
