786bb179f3996c01a9248b023bd246bdcf993137
bret jensen <jensenbret13@gmail.com>
2025-02-27 20:01:14 -0800

    getting started on styling

---
+++ src/components/Projects.jsx
@@ -0,0 +1,2 @@
+// TODO: create card elements to showcase previous projects
+// TODO: do i need a seperate projects.jsx file for each project, or can i style this one file, and import into the project.jsx file?
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -3,17 +3,17 @@
 import React from 'react';
 import Header from './Header'; 
 import Footer from './Footer'; 
+import './styles.css';
 
 function AboutMe() {
     return (
-        <div className="">
+        <div className="body">
             <Header />
             <main>
                 <h1>About Me</h1>
                 <img 
                     src=""
                     alt=""
-                    className=""
                 />
                 <p>
                     textextextext
