ef2e1bcc2519b6ada0bc72c12da51fd07f99b62f
bret jensen <jensenbret13@gmail.com>
2025-03-04 18:09:12 -0800

    .

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -3,7 +3,7 @@ import Projects from '../components/Projects.jsx';
 import image1 from '../assets/circuit.png';
 import image2 from '../assets/abstractcircuit.png';
 import image3 from '../assets/codeBunch.png'
-
+// TODO: import more images, and put to respective cards
 const style = {
     margin: '20px',
     fontSize: '1.75rem',
@@ -38,6 +38,7 @@ function Project() {
             image={image3}
             link="https://github.com/bretaj"
             description="textextext" />
+            {/* TODO: change linkes to actual projects from GitHub */}
             </div>
             </>
     );
