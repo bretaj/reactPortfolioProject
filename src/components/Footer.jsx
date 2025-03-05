6230e72aca3c63b392aca7ccd3110df1b7ca5a33
bret jensen <jensenbret13@gmail.com>
2025-03-04 20:04:17 -0800

    basic contact form styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,23 @@
 import { Link } from "react-router-dom";
-function Footer () {
+
+function Footer() {
     return (
-      <footer>
-        <nav>
-                  <ul>
-                      <li>
-                          <Link to="/">GitHub</Link>
-                      </li>
-                      <li>
-                          <Link to="/">LinkedIn</Link>
-                      </li>
-                      <li>
-                          <Link to="/">BlueSky</Link>
-                      </li>
-                  </ul>
-              </nav>
+        <footer>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">GitHub</Link>
+                    </li>
+                    <li>
+                        <Link to="/">LinkedIn</Link>
+                    </li>
+                    <li>
+                        <Link to="/">BlueSky</Link>
+                    </li>
+                </ul>
+            </nav>
         </footer>
     );
-  };
-  
-  export default Footer;
+};
+
+export default Footer;
